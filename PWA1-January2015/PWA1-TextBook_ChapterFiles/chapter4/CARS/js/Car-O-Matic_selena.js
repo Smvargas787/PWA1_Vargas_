d8d09923ce22f79e943bcce03459e78b8d3fc0c9
Smvargas787 <RicanChica@RicanChicas-MacBook-Pro.local>
2015-01-16 18:33:25 -0500

    finished objects

--- PWA1-January2015/PWA1-TextBook_ChapterFiles/chapter4/CARS/js/Car-O-Matic_selena.js
+++ PWA1-January2015/PWA1-TextBook_ChapterFiles/chapter4/CARS/js/Car-O-Matic_selena.js
@@ -3,8 +3,8 @@
  */
 //Selena's Car-O-Matic
 
-function makeCar() {
-    var makes = ["Chevy", "GM", "Fiat", "Webville Motors", "Tucker"];
+/*function makeCar() {
+    var makes =["Chevy","GM","Fiat","Webville Motors","Tucker"];
     var models = ["Cadillac", "500", "Bel-Air","Taxi", "Torpedo"];
     var years = ["1955","1957", "1948", "1954","1961"];
     var colors = ["blue" , "red" , "tan" , "white"];
@@ -62,3 +62,101 @@ var carToSell = makeCar();
 displayCar((carToSell));
 
 //makePhrases();
+*/
+
+var makes = {
+
+    key1:"Toyota",
+    key2:"Nissan",
+    key3:"Dodge",
+    key4:"Mitsubishi",
+    key5:"Honda"
+};
+
+var models = {
+    key1:"Skyline",
+    key2:"Mustang",
+    key3:"Eclipse",
+    key4:"Lamborghini",
+    key5:"Bentley"
+};
+
+var years = {
+    key1:"2020",
+    key2:"1995",
+    key3:"1959",
+    key4:"1823",
+    key5:"3001"
+};
+
+
+
+var colors = {
+    key1:"Blue",
+    key2:"Red",
+    key3:"Silver",
+    key4:"Pink",
+    key5:"Black"
+};
+
+var convertible = {
+    key1:true,
+    key2:false
+};
+
+function makeCar() {
+    var rand1 = Math.floor(Math.random() *Object.keys(makes).length)+1;
+    var rand2 = Math.floor(Math.random() *Object.keys(models).length)+1;
+    var rand3 = Math.floor(Math.random() *Object.keys(years).length)+1;
+    var rand4 = Math.floor(Math.random() *Object.keys(colors).length)+1;
+    var rand5 = Math.floor(Math.random() * 5) +1;
+    var rand6 = Math.floor(Math.random() * 2);
+
+
+    var randomKeys1 = "key" +rand1;
+    var randomKeys2 = "key" +rand2;
+    var randomKeys3 = "key" +rand3;
+    var randomKeys4 = "key" +rand4;
+    var randomKeys5 = "key" +rand5;
+    var randomKeys6 = "key" +rand6;
+
+    var car = {
+
+        make: makes[randomKeys1],
+        model: models[randomKeys2],
+        year: years[randomKeys3],
+        color: colors[randomKeys4],
+        passengers:[randomKeys5],
+        convertible: convertible[randomKeys6],
+        mileage: 0
+    };
+
+    return car;
+
+}makeCar();
+
+
+
+function displayCar(car) {
+
+    console.log("MAKES has this many Keys: " +Object.keys(makes).length);
+    console.log("MODELS has this many Keys: " +Object.keys(models).length);
+    console.log("YEARS has this many Keys: " +Object.keys(years).length);
+    console.log("COLORS has this many Keys: " +Object.keys(colors).length);
+    console.log("CONVERTIBLE has this many Keys: " +Object.keys(convertible).length);
+
+
+
+    document.write("<br>"+"MAKES has this many Keys: " +Object.keys(makes).length);
+    document.write("<br>"+"MODELS has this many Keys: " +Object.keys(models).length);
+    document.write("<br>"+"YEARS has this many Keys: " +Object.keys(years).length);
+    document.write("<br>"+"COLORS has this many Keys: " +Object.keys(colors).length);
+    document.write("<br>"+"CONVERTIBLE has this many Keys: " +Object.keys(convertible).length);
+}
+
+var carToSell = makeCar();
+
+displayCar(carToSell);
+
+
+
